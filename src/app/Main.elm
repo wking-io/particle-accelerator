2bbdce341c7bcc23c09eb4930617f761b8d77e22
Will King <contact@wking.io>
2018-07-28 10:08:26 -0500

    Setup init particle version

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,71 +1,188 @@
 module Main exposing (main)
 
-import Data.UserSession as UserSession exposing (UserSession)
-import Json.Encode exposing (Value)
-import Html exposing (Html, program, text, div, ul, li, img, p)
-
-
--- import Page.Admin as Admin
--- import Page.Memory.Game as MemoryGame
-
-import Page.Test as Test
-import Page.NotFound as NotFound
-import Page.Errored as Errored exposing (PageLoadError)
-import Navigation exposing (Location)
-import Route exposing (Route)
-import Util.Infix exposing ((=>))
+import AnimationFrame
+import Color exposing (Color)
+import Color.Convert exposing (colorToHex)
+import Html exposing (Html)
+import Json.Decode as Decode exposing (Decoder, Value)
 import Random exposing (Generator)
-import View.Page as Page
-import Task
+import Random.Extra
+import Random.List
+import Svg exposing (Svg)
+import Svg.Attributes as SA
+import Task exposing (Task)
+import Time exposing (Time)
+import Window exposing (Size)
 
 
-type Page
-    = Blank
-    | NotFound
-    | Errored PageLoadError
-      -- | Admin Admin.Model
-      -- | AdminSelected Admin.Model
-      -- | AdminSetup Admin.Model
-      -- | MemoryGame MemoryGame.Model
-    | Test Test.Model
+type alias Canvas =
+    { width : Int
+    , height : Int
+    }
 
 
-type PageState
-    = Loaded Page
-    | TransitioningFrom Page
+type alias Particle =
+    { origin : ( Float, Float )
+    , cx : Float
+    , cy : Float
+    , vx : Float
+    , vy : Float
+    , r : Float
+    , rMax : Float
+    , friction : Float
+    , gravity : Float
+    , color : Color
+    , dying : Bool
+    , duration : Float
+    }
 
 
-getPage : PageState -> Page
-getPage pageState =
-    case pageState of
-        Loaded page ->
-            page
+generateRadius : Particle -> Generator Float
+generateRadius { r } =
+    Random.float r (r + 3)
 
-        TransitioningFrom page ->
-            page
+
+generateHeading : Generator Float
+generateHeading =
+    Random.float (degrees 0) (degrees 360)
+
+
+generateColor : Generator Color
+generateColor =
+    Random.List.choose
+        [ Color.rgb 0 48 73
+        , Color.rgb 73 214 40
+        , Color.rgb 247 127 0
+        , Color.rgb 252 191 73
+        , Color.rgb 234 226 183
+        ]
+        |> Random.map Tuple.first
+        |> Random.map (Maybe.withDefault (Color.rgb 252 191 73))
+
+
+generateParticle : Particle -> Generator Particle
+generateParticle particle =
+    Random.map3
+        (\newR heading newColor -> setHeading heading { particle | rMax = newR, color = newColor })
+        (generateRadius particle)
+        generateHeading
+        generateColor
+
+
+getSpeed : Particle -> Float
+getSpeed { vx, vy } =
+    sqrt (vx * vx + vy * vy)
+
+
+setHeading : Float -> Particle -> Particle
+setHeading heading particle =
+    let
+        speed =
+            getSpeed particle
+    in
+        { particle
+            | vx = cos heading * speed
+            , vy = sin heading * speed
+        }
+
+
+isOffCanvas : Canvas -> Particle -> Bool
+isOffCanvas { height } { cy } =
+    (cy < 0) || (cy > toFloat height)
+
+
+isDead : Particle -> Bool
+isDead { r } =
+    r < 1
+
+
+isGrowing : Particle -> Bool
+isGrowing { r, rMax } =
+    r < rMax
 
 
 
 -- MODEL --
 
 
-type alias Model =
-    { session : UserSession
-    , pageState : PageState
-    }
-
-
-init : Value -> Location -> ( Model, Cmd Msg )
-init val location =
-    setRoute (Route.fromLocation location)
-        { pageState = Loaded initialPage
-        , session = UserSession.decoder val
+type Model
+    = Init Canvas
+    | Go
+        { canvas : Canvas
+        , particles : List Particle
         }
 
 
-initialPage : Page
-initialPage =
-    Blank
+init : Value -> ( Model, Cmd Msg )
+init json =
+    let
+        ( size, points ) =
+            case Decode.decodeValue flagDecoder json of
+                Ok result ->
+                    result
+
+                Err _ ->
+                    ( ( 0, 0 ), [] )
+
+        canvas =
+            { width = Tuple.first size, height = Tuple.second size }
+    in
+        ( Init canvas
+        , points
+            |> List.map pointToParticle
+            |> List.map (updateParticle True canvas)
+            |> Random.Extra.combine
+            |> Random.generate Generated
+        )
+
+
+flagDecoder : Decoder ( ( Int, Int ), List Point )
+flagDecoder =
+    Decode.map2 (,)
+        (Decode.at [ "size" ] sizeDecoder)
+        (Decode.at [ "points" ] (Decode.list pointDecoder))
+
+
+sizeDecoder : Decoder ( Int, Int )
+sizeDecoder =
+    Decode.map2 (,)
+        (Decode.at [ "width" ] Decode.int)
+        (Decode.at [ "height" ] Decode.int)
+
+
+pointDecoder : Decoder Point
+pointDecoder =
+    Decode.list Decode.int
+        |> Decode.andThen
+            (\list ->
+                case list of
+                    [ x, y ] ->
+                        Decode.succeed ( x, y )
+
+                    _ ->
+                        Decode.fail "Did not recieve a pair"
+            )
+
+
+type alias Point =
+    ( Int, Int )
+
+
+pointToParticle : Point -> Particle
+pointToParticle ( cx, cy ) =
+    { origin = ( toFloat cx, toFloat cy )
+    , cx = toFloat cx
+    , cy = toFloat cy
+    , vx = 0
+    , vy = 0
+    , r = 2
+    , rMax = 5
+    , friction = 0.99
+    , gravity = 0
+    , color = Color.blue
+    , dying = False
+    , duration = 0.2
+    }
 
 
 
@@ -74,55 +191,30 @@ initialPage =
 
 view : Model -> Html Msg
 view model =
-    case model.pageState of
-        Loaded page ->
-            viewPage False page
+    case model of
+        Init _ ->
+            Html.text ""
 
-        TransitioningFrom page ->
-            viewPage True page
+        Go { canvas, particles } ->
+            Html.div []
+                [ Svg.svg
+                    [ SA.width (toString canvas.width)
+                    , SA.height (toString canvas.height)
+                    , SA.viewBox ("0 0 " ++ toString canvas.width ++ " " ++ toString canvas.height)
+                    ]
+                    (List.map viewParticle particles)
+                ]
 
 
-viewPage : Bool -> Page -> Html Msg
-viewPage isLoading page =
-    let
-        frame =
-            Page.frame isLoading
-    in
-        case page of
-            NotFound ->
-                NotFound.view
-                    |> frame
-
-            Blank ->
-                -- This is for the very initial page load, while we are loading
-                -- data via HTTP. We could also render a spinner here.
-                Html.text ""
-                    |> frame
-
-            Errored subModel ->
-                Errored.view subModel
-                    |> frame
-
-            -- Admin subModel ->
-            --     Admin.view subModel
-            --         |> frame
-            --         |> Html.map AdminMsg
-            -- AdminSelected subModel ->
-            --     Admin.view subModel
-            --         |> frame
-            --         |> Html.map AdminMsg
-            -- AdminSetup subModel ->
-            --     Admin.view subModel
-            --         |> frame
-            --         |> Html.map AdminMsg
-            -- MemoryGame subModel ->
-            --     MemoryGame.view subModel
-            --         |> frame
-            --         |> Html.map MemoryGameMsg
-            Test subModel ->
-                Test.view subModel
-                    |> frame
-                    |> Html.map TestMsg
+viewParticle : Particle -> Svg Msg
+viewParticle { cx, cy, r, color } =
+    Svg.circle
+        [ SA.fill (colorToHex color)
+        , SA.cx (toString cx)
+        , SA.cy (toString cy)
+        , SA.r (toString r)
+        ]
+        []
 
 
 
@@ -130,126 +222,95 @@ viewPage isLoading page =
 
 
 type Msg
-    = SetRoute (Maybe Route)
-      -- | AdminLoaded (Result PageLoadError Admin.Model)
-    | TestLoaded (Result PageLoadError Test.Model)
-      -- | MemoryGameLoaded MemoryGame.Model
-      -- | AdminMsg Admin.Msg
-      -- | MemoryGameMsg MemoryGame.Msg
-    | TestMsg Test.Msg
-
-
-setRoute : Maybe Route -> Model -> ( Model, Cmd Msg )
-setRoute maybeRoute model =
-    let
-        generate msg result =
-            case result of
-                Ok generator ->
-                    { model | pageState = TransitioningFrom (getPage model.pageState) }
-                        => Random.generate msg generator
-
-                Err error ->
-                    { model | pageState = Loaded (Errored error) } => Cmd.none
-
-        isError page init =
-            case init of
-                Ok subModel ->
-                    { model | pageState = Loaded (page subModel) } => Cmd.none
-
-                Err error ->
-                    { model | pageState = Loaded (Errored error) } => Cmd.none
-
-        errored =
-            pageErrored model
-
-        transition toMsg task =
-            { model | pageState = TransitioningFrom (getPage model.pageState) }
-                => Task.attempt toMsg task
-    in
-        case maybeRoute of
-            Nothing ->
-                { model | pageState = Loaded NotFound } => Cmd.none
-
-            Just Route.Root ->
-                transition TestLoaded (Test.init model.session)
-
-            -- Just (Route.Admin slug) ->
-            --     isError Admin (Admin.init slug False)
-            -- Just (Route.AdminSetup slug) ->
-            --     isError Admin (Admin.init slug True)
-            -- Just (Route.MemoryGame slug) ->
-            --     generate MemoryGameLoaded (MemoryGame.init slug)
-            Just Route.Test ->
-                transition TestLoaded (Test.init model.session)
-
-
-pageErrored : Model -> String -> ( Model, Cmd msg )
-pageErrored model errorMessage =
-    let
-        error =
-            Errored.pageLoadError errorMessage
-    in
-        { model | pageState = Loaded (Errored error) } => Cmd.none
+    = Tick Time
+    | Generated (List Particle)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    updatePage (getPage model.pageState) msg model
+    case msg of
+        Tick _ ->
+            case model of
+                Init _ ->
+                    ( model, Cmd.none )
+
+                Go { canvas, particles } ->
+                    ( model
+                    , Random.generate
+                        Generated
+                        (particles
+                            |> List.map (updateParticle False canvas)
+                            |> Random.Extra.combine
+                        )
+                    )
+
+        Generated particles ->
+            case model of
+                Init canvas ->
+                    ( Go { canvas = canvas, particles = particles }, Cmd.none )
+
+                Go { canvas } ->
+                    ( Go { canvas = canvas, particles = particles }, Cmd.none )
 
 
-updatePage : Page -> Msg -> Model -> ( Model, Cmd Msg )
-updatePage page msg model =
-    let
-        toPage toModel toMsg subUpdate subMsg subModel =
-            let
-                ( newModel, newCmd ) =
-                    subUpdate subMsg subModel
-            in
-                ( { model | pageState = Loaded (toModel newModel) }, Cmd.map toMsg newCmd )
-
-        errored =
-            pageErrored model
-    in
-        case ( msg, page ) of
-            ( SetRoute route, _ ) ->
-                setRoute route model
-
-            -- ( AdminLoaded (Ok subModel), _ ) ->
-            --     { model | pageState = Loaded (Admin subModel) } => Cmd.none
-            -- ( AdminLoaded (Err error), _ ) ->
-            --     { model | pageState = Loaded (Errored error) } => Cmd.none
-            ( TestLoaded (Ok subModel), _ ) ->
-                { model | pageState = Loaded (Test subModel) } => Cmd.none
-
-            ( TestLoaded (Err error), _ ) ->
-                { model | pageState = Loaded (Errored error) } => Cmd.none
-
-            -- ( MemoryGameLoaded subModel, _ ) ->
-            --     { model | pageState = Loaded (MemoryGame subModel) } => Cmd.none
-            -- ( AdminMsg subMsg, Admin subModel ) ->
-            --     toPage Admin AdminMsg Admin.update subMsg subModel
-            -- ( MemoryGameMsg subMsg, MemoryGame subModel ) ->
-            --     toPage MemoryGame MemoryGameMsg MemoryGame.update subMsg subModel
-            ( _, NotFound ) ->
-                -- Disregard incoming messages when we're on the
-                -- NotFound page.
-                model => Cmd.none
-
-            ( _, _ ) ->
-                -- Disregard incoming messages that arrived for the wrong page
-                model => Cmd.none
+updateParticle : Bool -> Canvas -> Particle -> Generator Particle
+updateParticle isInit canvas particle =
+    if isInit then
+        generateParticle
+            { particle
+                | cx = Tuple.first particle.origin
+                , cy = Tuple.second particle.origin
+                , vx = particle.vx
+                , vy = particle.vy
+                , r = 2
+                , dying = False
+            }
+    else
+        particle
+            |> updateHealth
+            |> updatePosition canvas
 
 
+updatePosition : Canvas -> Particle -> Generator Particle
+updatePosition canvas particle =
+    if isOffCanvas canvas particle || isDead particle then
+        generateParticle
+            { particle
+                | cx = Tuple.first particle.origin
+                , cy = Tuple.second particle.origin
+                , vx = particle.vx
+                , vy = particle.vy
+                , r = 2
+                , dying = False
+            }
+    else
+        Random.Extra.constant
+            { particle
+                | cx = particle.cx + particle.vx
+                , cy = particle.cy + particle.vy
+                , vx = particle.vx * particle.friction
+                , vy = particle.vy * particle.friction
+            }
 
--- SUBSCRIPTIONS
--- MAIN
+
+updateHealth : Particle -> Particle
+updateHealth particle =
+    if isGrowing particle && not particle.dying then
+        { particle
+            | r = particle.r + particle.duration
+        }
+    else
+        { particle
+            | r = particle.r - particle.duration
+            , dying = True
+        }
 
 
 main : Program Value Model Msg
 main =
-    Navigation.programWithFlags (Route.fromLocation >> SetRoute)
+    Html.programWithFlags
         { init = init
         , view = view
         , update = update
-        , subscriptions = \model -> Sub.none
+        , subscriptions = \_ -> AnimationFrame.times Tick
         }
